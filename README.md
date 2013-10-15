e2b3c8e9c5e031a11b872c6174a47dfbc22237dc
SG <sy1840@gmail.com>
2013-10-15 14:14:55 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an app based off of the Railscast episode #250 that integrates the Devise gem for managing user access and logins.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = 'e59e8ca97e45f8db735c61507cc5df14a8724967f627e5a267753e3b5d21271e01b24b4b32281e264d5fe1abc000538d59e5e40a63bc9586fd9bf9134d109904'
